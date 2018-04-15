e566e427adc97fc160d00537b4246b1bff4c3b21
alia-b1 <alia-b@hotmail.co.uk>
2018-04-15 14:06:54 +0100

    Completed administrator model
    still need to change exception handling and double check once views completed

--- models/administrator.php
+++ models/administrator.php
@@ -5,22 +5,22 @@ include_once'user.php';
  
 class Administrator extends User { 
  
-//    public static function all() { 
-//      $list = []; 
-//      $db = Db::getInstance(); 
-//      $req = $db->query('SELECT * FROM blog_user WHERE user_type_id = :2'); 
-//      // we create a list of Product objects from the database results 
-//      foreach($req->fetchAll() as $blogger) { 
-//        $list[] = new Blogger($blogger['$id'],$blogger['first_name'], $blogger['last_name'], $blogger['dob'], $blogger['date_joined'], $blogger['password'],$blogger['email']); 
-//      } 
-//      return $list; 
-//    } should only be for administrator 
+    public static function all() {
+      $list = [];
+      $db = Db::getInstance();
+      $req = $db->query('SELECT * FROM blog_user');
+      // we create a list of Product objects from the database results
+      foreach($req->fetchAll() as $blogger) {
+        $list[] = new Administrator($administrator['$id'],$administrator['first_name'], $administrator['last_name'], $administrator['dob'], $administrator['date_joined'], $administrator['password'],$administrator['email']);
+      }
+      return $list;
+    } 
  
     public static function find($id) { 
       $db = Db::getInstance(); 
       //use intval to make sure $id is an integer 
       $id = intval($id); 
-      $req = $db->prepare('SELECT * FROM blog_user WHERE id = :id, user_type_id = 1'); 
+      $req = $db->prepare('SELECT * FROM blog_user WHERE id = :id'); 
       //the query was prepared, now replace :id with the actual $id value 
       $req->execute(array('id' => $id)); 
       $administrator = $req->fetch(); 
@@ -34,63 +34,83 @@ if($administrator){
     } 
     } 
  
-public static function update($email, $first_name, $last_name, $password, $dob) { 
-    $db = Db::getInstance(); 
-    $sessionemail=$_POST['email']; 
-    $req = $db->prepare("Update blog_user set email=:email, first_name=:first_name last_name=:last_name, password=:password, dob=:dob where email=$sessionemail"); 
-    $req->bindParam(':email', $email); 
-    $req->bindParam(':first_name', $first_name); 
-    $req->bindParam(':last_name', $last_name); 
-    $req->bindParam(':password', $password); 
-    $req->bindParam(':dob', $dob); 
- 
-// set name and price parameters and execute 
-    if(isset($_POST['email'])&& $_POST['email']!=""){ 
-        $filteredEmail = filter_input(INPUT_POST,'email', FILTER_SANITIZE_SPECIAL_CHARS); 
-    } 
-    if(isset($_POST['first_name'])&& $_POST['first_name']!=""){ 
-        $filteredFirstName = filter_input(INPUT_POST,'first_name', FILTER_SANITIZE_SPECIAL_CHARS); 
-    } 
-    if(isset($_POST['last_name'])&& $_POST['last_name']!=""){ 
-        $filteredLastName = filter_input(INPUT_POST,'last_name', FILTER_SANITIZE_SPECIAL_CHARS); 
-    } 
-    if(isset($_POST['password'])&& $_POST['password']!=""){ 
-        $filteredPassword = filter_input(INPUT_POST,'password', FILTER_SANITIZE_SPECIAL_CHARS); 
-    } 
-    if(isset($_POST['dob'])&& $_POST['dob']!=""){ 
-        $filteredDOB = filter_input(INPUT_POST,'dob', FILTER_SANITIZE_SPECIAL_CHARS); 
-    } 
-     
-$email = $filteredEmail; 
-$first_name = $filteredFirstName; 
-$last_name = $filteredLastName; 
-$password = $filteredPassword; 
-$dob = $filteredDOB; 
-$req->execute(); 
- 
-//upload product image if it exists 
-        if (!empty($_FILES[self::InputKey]['name'])) { 
-    Product::uploadFile($name); 
-  } 
- 
-    } 
-     
-    public static function add() { 
-    $db = Db::getInstance(); 
-    $req = $db->prepare("Insert into product(name, price) values (:name, :price)"); 
-    $req->bindParam(':name', $name); 
-    $req->bindParam(':price', $price); 
+public static function update($email, $first_name, $last_name, $password, $dob) {
+    $db = Db::getInstance();
+    $sessionemail=$_POST['email'];
+    $req = $db->prepare("Update blog_user set email=:email, first_name=:first_name last_name=:last_name, password=:password, dob=:dob where id=:id");
+    $req->bindParam(':email', $email);
+    $req->bindParam(':first_name', $first_name);
+    $req->bindParam(':last_name', $last_name);
+    $req->bindParam(':password', $password);
+    $req->bindParam(':dob', $dob);
+
+// set name and price parameters and execute
+    if(isset($_POST['email'])&& $_POST['email']!=""){
+        $filteredEmail = filter_input(INPUT_POST,'email', FILTER_SANITIZE_SPECIAL_CHARS);
+    }
+    if(isset($_POST['first_name'])&& $_POST['first_name']!=""){
+        $filteredFirstName = filter_input(INPUT_POST,'first_name', FILTER_SANITIZE_SPECIAL_CHARS);
+    }
+    if(isset($_POST['last_name'])&& $_POST['last_name']!=""){
+        $filteredLastName = filter_input(INPUT_POST,'last_name', FILTER_SANITIZE_SPECIAL_CHARS);
+    }
+    if(isset($_POST['password'])&& $_POST['password']!=""){
+        $filteredPassword = filter_input(INPUT_POST,'password', FILTER_SANITIZE_SPECIAL_CHARS);
+    }
+    if(isset($_POST['dob'])&& $_POST['dob']!=""){
+        $filteredDOB = filter_input(INPUT_POST,'dob', FILTER_SANITIZE_SPECIAL_CHARS);
+    }
+    
+$email = $filteredEmail;
+$first_name = $filteredFirstName;
+$last_name = $filteredLastName;
+$password = $filteredPassword;
+$dob = $filteredDOB;
+$req->execute();
+
+//upload product image if it exists
+        if (!empty($_FILES[self::InputKey]['name'])) {
+		Product::uploadFile($name);
+	}
+
+    }
+    
+public static function add() {
+    $db = Db::getInstance();
+    $req = $db->prepare("Insert into blog_user(first_name, last_name,dob, date_joined, password, email,user_type_id) values (:first_name,:last_name,:dob,:date_joined,:password,:email,:user_type_id)");
+    $req->bindParam(':first_name', $first_name);
+    $req->bindParam(':last_name', $last_name);
+    $req->bindParam(':dob', $dob);
+    $req->bindParam(':date_joined', $date_joined);
+    $req->bindParam(':password', $password);
+    $req->bindParam(':email', $email);
+    $req->bindParam(':user_type_id', $user_type_id);
+    $req->bindParam(':last_name', $last_name);
+
  
 // set parameters and execute 
-    if(isset($_POST['name'])&& $_POST['name']!=""){ 
-        $filteredName = filter_input(INPUT_POST,'name', FILTER_SANITIZE_SPECIAL_CHARS); 
-    } 
-    if(isset($_POST['price'])&& $_POST['price']!=""){ 
-        $filteredPrice = filter_input(INPUT_POST,'price', FILTER_SANITIZE_SPECIAL_CHARS); 
-    } 
-$name = $filteredName; 
-$price = $filteredPrice; 
-$req->execute(); 
+    if(isset($_POST['email'])&& $_POST['email']!=""){
+        $filteredEmail = filter_input(INPUT_POST,'email', FILTER_SANITIZE_SPECIAL_CHARS);
+    }
+    if(isset($_POST['first_name'])&& $_POST['first_name']!=""){
+        $filteredFirstName = filter_input(INPUT_POST,'first_name', FILTER_SANITIZE_SPECIAL_CHARS);
+    }
+    if(isset($_POST['last_name'])&& $_POST['last_name']!=""){
+        $filteredLastName = filter_input(INPUT_POST,'last_name', FILTER_SANITIZE_SPECIAL_CHARS);
+    }
+    if(isset($_POST['password'])&& $_POST['password']!=""){
+        $filteredPassword = filter_input(INPUT_POST,'password', FILTER_SANITIZE_SPECIAL_CHARS);
+    }
+    if(isset($_POST['dob'])&& $_POST['dob']!=""){
+        $filteredDOB = filter_input(INPUT_POST,'dob', FILTER_SANITIZE_SPECIAL_CHARS);
+    }
+    
+$email = $filteredEmail;
+$first_name = $filteredFirstName;
+$last_name = $filteredLastName;
+$password = $filteredPassword;
+$dob = $filteredDOB;
+$req->execute();
  
 //upload product image 
 Product::uploadFile($name); 
