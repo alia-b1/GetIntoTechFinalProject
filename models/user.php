8c56fc12d84a7abb045b4a2b87a252c0edea645f
Unknown <lara_phillips@hotmail.com>
2018-04-16 21:00:02 +0100

    login now working

--- models/user.php
+++ models/user.php
@@ -1,26 +1,51 @@
 <?php
-abstract class User {
 
-    protected $id;
-    protected $first_name;
-    protected $last_name;
-    protected $dob;
-    protected $date_joined;
-    protected $password;
-    protected $email;
-    protected $user_type_id;
-
-    public function __construct($id, $first_name, $last_name, $dob,$date_joined,$password, $email, $user_type_id) {
-        $this->ID = $id;
-        $this->firstName = $first_name;
-        $this->lastName = $last_name;
-        $this->dob = $dob;
-        $this->dateJoined = $date_joined;
-        $this->password = $password;
-        $this->username = $email;
-        $this->user_type_id = $user_type_id;
-        
+class Login {
+    protected $ID;
+    public $first_name;
+    public $last_name;
+    protected $user_type;
+                
+    function __construct($ID, $first_name, $last_name, $user_type) {
+        $this->ID = $ID;
+        $this->first_name = $first_name;
+        $this->last_name = $last_name;
+        $this->user_type = $user_type;
     }
 
-
+    public function loginUser($emailinput, $passwordinput) {
+        $db = Db::getInstance();
+        $req = $db->prepare("SELECT u.ID, u.first_name, u.last_name, t.type FROM blog_user as u INNER JOIN user_type as t ON u.user_type_id=t.ID WHERE u.email= :email AND u.password= :password");
+        $req->bindParam(':email', $email);
+        $req->bindParam(':password', $password);
+        try {
+            if(isset($_POST['email'])&& $_POST['email']!=""){
+                 $filteredEmail = filter_input(INPUT_POST,'email', FILTER_SANITIZE_SPECIAL_CHARS);
+            }    
+            if(isset($_POST['password'])&& $_POST['password']!=""){
+                 $filteredPassword = filter_input(INPUT_POST,'password', FILTER_SANITIZE_SPECIAL_CHARS);
+            }
+            $email = $filteredEmail;
+            $password = $filteredPassword;
+            $req->execute();
+            $user=$req->fetch();
+            if ($user){
+                return new Login($user['ID'],$user['first_name'], $user['last_name'], $user['type']);
+            }
+//            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+//                $_SESSION["id"]=$row["ID"];
+//                $_SESSION["firstName"]=$row["first_name"];
+//                $_SESSION["lastName"]=$row["last_name"];
+//                if($row["type"] =='librarian'){
+//                    echo $row["type"];
+//                    header("Location:librarianAccountPage.php");
+//                }else{
+//                    header("Location:memberAccountPage.php");
+//                }
+//            }
+        } catch (PDOException $e) {
+            die("Could not login ....." . $e->getMessage());
+        }
+        unset($stmt);
+    }
 }
