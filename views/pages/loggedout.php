1e121e8fcbafbbea74f7d6bf28c945f1dd4b139d
Laura <lauralizziedavis@gmail.com>
2018-04-17 22:00:30 +0100

    created about, contact, logged out pages, added session_start() to layou

---
+++ views/pages/loggedout.php
@@ -0,0 +1,12 @@
+
+<div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
+  <div class="col-md-12 px-0">
+    <h1 class="display-4 font-italic" align="center">BLAH BLAH LAND</h1>
+  </div>
+</div>
+
+
+      <h3>Bye bye baby!
+      </h3>
+
+Thanks for logging out; you'll be back!
