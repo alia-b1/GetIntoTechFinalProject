6b4d85381282fe7b23c8889b0feed882c29291ce
Penny <pfordred@hotmail.co.uk>
2018-04-17 21:19:59 +0100

    Updated Blog Post display

--- models/blog.php
+++ models/blog.php
@@ -1,27 +1,36 @@
 <?php
 class Blog {
-    public $id; 
+    public $ID; 
     public $title;
     public $dateCreated;
     protected $user_ID;
     public $category;
     public $content;
+    public $movie_title;
+    public $release_year;
+    public $director;
+    public $movie_poster;
     
-    public function __construct($ID, $title, $date_created, $user_id, $movie_id, $content) {
+    public function __construct($ID, $title, $date_created, $user_id, $movie_id, $content, $movie_title, $release_year, $director, $movie_poster) {
       $this->ID= $ID;
       $this->title = $title;
       $this->date_created  = $date_created;
       $this->user_id = $user_id;
       $this->movie_id = $movie_id;
       $this->content = $content;
+      $this->movie_title = $movie_title;
+      $this->release_year = $release_year;
+      $this->director = $director;
+      $this->movie_poster = $movie_poster;
+      
     }
       public static function all() {
       $list = [];
       $db = Db::getInstance();
-      $req = $db->query('SELECT * FROM blog');
+      $req = $db->query('SELECT blog.id, blog.title, blog.date_created, blog.user_id, blog.movie_id, blog.content, movie.movie_title, movie.release_year, movie.director, movie.movie_poster FROM blog INNER JOIN movie ON blog.movie_ID = movie.ID');
       // we create a list of Product objects from the database results
       foreach($req->fetchAll() as $blog) {
-        $list[] = new Blog ($blog['ID'], $blog['title'], $blog['date_created'], $blog['user_id'], $blog['movie_id'], $blog['content']);
+        $list[] = new Blog ($blog['id'], $blog['title'], $blog['date_created'], $blog['user_id'], $blog['movie_id'], $blog['content'], $blog['movie_title'], $blog['release_year'], $blog['director'], $blog['movie_poster']);
       }
       return $list;
     }
@@ -30,12 +39,12 @@ class Blog {
       $db = Db::getInstance();
       //use intval to make sure $id is an integer
       $ID = intval($ID);
-      $req = $db->prepare('SELECT * FROM blog WHERE ID = :ID');
+      $req = $db->prepare("SELECT blog.id, blog.title, blog.date_created, blog.user_id, blog.movie_id, blog.content, movie.movie_title, movie.release_year, movie.director, movie.movie_poster FROM blog INNER JOIN movie ON blog.movie_ID = movie.ID WHERE blog.ID = :ID");
       //the query was prepared, now replace :id with the actual $id value
       $req->execute(array('ID' => $ID));
       $blog = $req->fetch();
 if($blog){
-      return new Blog ($blog['ID'], $blog['title'], $blog['date_created'], $blog['user_id'], $blog['movie_id'], $blog['content']);
+      return new Blog ($blog['id'], $blog['title'], $blog['date_created'], $blog['user_id'], $blog['movie_id'], $blog['content'], $blog['movie_title'], $blog['release_year'], $blog['director'], $blog['movie_poster']);
     }
     else
     {
@@ -44,47 +53,6 @@ if($blog){
         throw new Exception('A real exception should go here');
     }
     }
-    
-public static function add() {
-     $db = Db::getInstance();
-    $req = $db->prepare("Insert into blog (title, date_created, user_ID, movie_id, content) values title=:title, date_created=:datetoday, user_ID=:UserID, movie_id:category, content:article");
-    $req->bindParam(':id', $ID);
-    $req->bindParam(':title', $title);
-    $req->bindParam(':datetoday', $date_created);
-    $req->bindParam(':UserID', $user_id);
-    $req->bindParam(':category', $movie_id);
-    $req->bindParam(':article', $content);
-    
-
-// set name and price parameters and execute
-    if(isset($_POST['name'])&& $_POST['name']!=""){
-        $filteredName = filter_input(INPUT_POST,'name', FILTER_SANITIZE_SPECIAL_CHARS);
-    }
-    if(isset($_POST['datetoday'])&& $_POST['datetoday']!=""){
-        $filteredDate = filter_input(INPUT_POST,'datetoday', FILTER_SANITIZE_SPECIAL_CHARS);
-    }
-    if(isset($_POST['UserID'])&& $_POST['UserID']!=""){
-        $filteredUserID = filter_input(INPUT_POST,'UserID', FILTER_SANITIZE_SPECIAL_CHARS);
-    }
-    if(isset($_POST['category'])&& $_POST['category']!=""){
-        $filteredCategory = filter_input(INPUT_POST,'category', FILTER_SANITIZE_SPECIAL_CHARS);
-    }
-    if(isset($_POST['article'])&& $_POST['article']!=""){
-        $filteredArticle = filter_input(INPUT_POST,'article', FILTER_SANITIZE_SPECIAL_CHARS);
-    }
-$title = $filteredName;
-$date_created = $filteredDate;
-$user_id = $filteredUserID;
-$movie_id = $filteredCategory;
-$content = $filteredArticle;
-
-$req->execute();
-//upload product image
-Product::uploadFile($name);
-    }
-
-const AllowedTypes = ['image/jpeg', 'image/jpg'];
-const InputKey = 'myUploader';
 }
 /* 
  * To change this license header, choose License Headers in Project Properties.
--- views/blog/read.php
+++ views/blog/read.php
@@ -2,6 +2,16 @@
 <p class="container"><?php echo $blog->content; ?></p>
 <h4>Publish Date: <?php echo $blog->date_created; ?></h4>
 <br>
+<br>
+<h3> About this movie </h3>
+    <?php echo "Film name: $blog->movie_title" ?>
+    <br>
+<?php echo "Director: $blog->director" ?>
+    <br>
+<?php echo "Release Year $blog->release_year" ?>
+    <br>
+<img src="<?php echo $blog->movie_poster?>" alt="Movie Image" style="width:250px;height:250px;">
+
 <a href='?controller=blog&action=delete&id=<?php echo $blog->ID; ?>'>Delete Blog Article</a> &nbsp; &nbsp;
 <br>
 <a href='?controller=blog&action=update&id=<?php echo $blog->ID; ?>'>Update Blog Article</a> &nbsp; &nbsp;
