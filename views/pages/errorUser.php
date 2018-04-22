75f1ef623d378dd798cf4449a8bfc4c3064cc218
Penny <pfordred@hotmail.co.uk>
2018-04-22 16:24:09 +0100

    Revert "Removed Victoria's files, updated formatting..."
    This reverts commit 13bb1ec8c2f3ef6df9defb971aaeb734d15bc241.

--- views/pages/errorUser.php
+++ views/pages/errorUser.php
@@ -1,171 +1,25 @@
+<!-- Title -->
 
-<!DOCTYPE html>
-<link href="../css/blog2.css" rel="stylesheet" type="text/css"/>
-<html lang="en">
-    
-<!-- Head -->
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1" >
-    <title>Error submitting blog</title>
+<title>Blog Submission Error</title>
 
-</head>
-    <!-- End of Head -->
+<!-- Logo -->
 
-    <!-- Navigation Bar -->
-<div id="navbar">
-    <a href="home.php">Home</a>
-    <a href="about.php">About</a>
-    <a href="contact.php">Contact</a>
-    <a href="login.php">Login</a>
-    <div class="dropdown">
-        <button class="dropbtn">Archive 
-        <i class="fa fa-caret-down"></i>
-        </button>
-        <div class="dropdown-content">
-        <a href="#">April 2018</a>
-        <a href="#">March 2018</a>
-        <a href="#">February 2018</a>
-        <a href="#">January 2018</a>b
-        <a href="#">December 2017</a></div></div>
-    <div class="topnav-right">
-    <a href="#" class="facebook"><i class="fa fa-facebook"></i></a> 
-    <a href="#" class="twitter"><i class="fa fa-twitter"></i></a> 
-    <a href="#" class="google"><i class="fa fa-google"></i></a> 
-    <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
-    <a href="#" class="youtube"><i class="fa fa-youtube"></i></a> </div>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <div class="icon-bar">
-    </div>
+<div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
+  <div class="col-md-12 px-0">
+    <h1 class="display-4 font-italic" align="center">BLAH BLAH LAND</h1>
+  </div>
 </div>
-<!-- End of Navigation Bar -->
-
-<!-- Wrapping -->
-<div class="big-wrapper">
 
 <!-- Header -->
-<div class="header-cover section bg-dark-light no-padding">
-    <div class="header section" style="background-image: url(https://images.unsplash.com/photo-1510511450816-30c68106b199?ixlib=rb-0.3.5&s=b8ec5e44eabbf55182615ee0e65be14e&auto=format&fit=crop&w=1950&q=80);">
-    <div class="header-inner section-inner">
-    <div class="blog-info">
-    <h2 class="blog-title"><a href="home" rel="home">Blah Blah Land</a></h2></div></div></div>
-</div>
-<!-- End of Header -->
 
-<!-- End of Wrapping -->
-</div>
+<h2>Blog Submission Error</h2>
 
-<!-- Wrapping -->
-<div class="wrapper section-inner">
+<!-- Content -->
 
-<!-- Article -->
-<div class="content left">
-    <div class="posts">
-    <div class="post-header">
-        <h1 class="post-title">Fiddlesticks!</a></h1>
-
-        <p>It looks like you are not registered or have entered the wrong information! Please register or re-enter your details.</p><br>
-
-<img src="../images/standard/MarilynOops.jpg" width="150" /><br><br>
-
+<div class="w3-container w3-red">
+    <h3>Submission failed!</h3>
+<p>Oops, this is the error page.</p>
+<p>It looks like you are not registered or have entered the wrong information! Please register or re-enter your details!</p>
 <a href="?controller=blog&action=create"><h3>Try again?</h3></a>
 
 </div>
-    
-<!-- End of Article -->
-
-    </div>
-<!-- End of Meta Tags -->
-
-    </div>
-<!-- End of Posts -->
-
-    </div>
-<!-- End of content to the left -->
-
-<!-- Sidebar -->
-<!-- Other Articles -->
-    <div class="sidebar right" role="complementary">
-    <div class="widget widget_recent_entries"><div class="widget-content">		
-            <h3 class="widget-title">Other Articles</h3>		
-            <ul>                                                                    
-            <li>                   
-                <a href="?controller=blog&action=read&id=17">Carrey On Jim</a><br>
-            <span class="post-date">03-04-2018</span>
-            </li>   
-            <li>
-                <a href="?controller=blog&action=read&id=15">Supergood, actually</a><br>
-            <span class="post-date">03-04-2018</span>
-            </li>   
-            <li>                   
-                <a href="?controller=blog&action=read&id=33">You can't sit with us...</a><br>
-            <span class="post-date">28-01-2018</span>
-            </li>                                       
-            <li>                   
-                <a href="?controller=blog&action=read&id=20">Vote for Pedro!</a><br>
-            <span class="post-date">04-12-2017</span>
-            </li>
-            </ul>
-        </div></div><div class="clear"></div>
-        
-<!-- End of other articles -->
-
-<!-- Categories -->
-    <div class="widget widget_tag_cloud"><div class="widget-content"><h3 class="widget-title">Categories</h3>
-    <div class="tagcloud"><a href="Insert link to category here">Romance</a>
-    <div class="tagcloud"><a href="Insert link to category here">Comedy</a>
-    <div class="tagcloud"><a href="Insert link to category here">Science Fiction</a>
-    <div class="tagcloud"><a href="Insert link to category here">Horror</a>
-    <div class="tagcloud"><a href="Insert link to category here">Action</a>
-    <div class="tagcloud"><a href="Insert link to category here">Thriller</a>
-    <div class="tagcloud"><a href="Insert link to category here">Drama</a>
-    <div class="tagcloud"><a href="Insert link to category here">Mystery</a>
-    <div class="tagcloud"><a href="Insert link to category here">Crime</a>
-    <div class="tagcloud"><a href="Insert link to category here">Animation</a>
-    <div class="tagcloud"><a href="Insert link to category here">Adventure</a>
-    <div class="tagcloud"><a href="Insert link to category here">Fantasy</a>
-
-    </div></div></div></div></div></div></div></div></div></div>
-    <div class="clear"></div>
-<!-- End of Categories -->
-
-    </div>
-<!-- End of Sidebar -->
-
-    </div>
-<!-- End of Wrapper -->
-    
-    <div class="clear"></div>
-<!-- End of page -->
-
-</body>
-</html>
-
-
-   
-
-
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-    <script src="../../../../assets/js/vendor/popper.min.js"></script>
-    <script src="../../../../dist/js/bootstrap.min.js"></script>
-    <script src="../../../../assets/js/vendor/holder.min.js"></script>
-    <script>
-      Holder.addTheme('thumb', {
-        bg: '#55595c',
-        fg: '#eceeef',
-        text: 'Thumbnail'
-      });
-    </script>
-    <br>
-        <footer class="blog-footer">
-      <p>
-        <a href="#">Back to top</a>
-      </p>
-    </footer>
-  </body>
-</html>
--- views/pages/register.php
+++ views/pages/register.php
@@ -1,70 +1,22 @@
+<!-- Title -->
 
-<!DOCTYPE html>
-<link href="../css/blog2.css" rel="stylesheet" type="text/css"/>
-<html lang="en">
-    
-<!-- Head -->
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1" >
-    <title>Register</title>
+<title>Register</title>
 
-</head>
-    <!-- End of Head -->
+<!-- Logo -->
 
-    <!-- Navigation Bar -->
-<div id="navbar">
-    <a href="home.php">Home</a>
-    <a href="about.php">About</a>
-    <a href="contact.php">Contact</a>
-    <a href="login.php">Login</a>
-    <div class="dropdown">
-        <button class="dropbtn">Archive 
-        <i class="fa fa-caret-down"></i>
-        </button>
-        <div class="dropdown-content">
-        <a href="#">April 2018</a>
-        <a href="#">March 2018</a>
-        <a href="#">February 2018</a>
-        <a href="#">January 2018</a>b
-        <a href="#">December 2017</a></div></div>
-    <div class="topnav-right">
-    <a href="#" class="facebook"><i class="fa fa-facebook"></i></a> 
-    <a href="#" class="twitter"><i class="fa fa-twitter"></i></a> 
-    <a href="#" class="google"><i class="fa fa-google"></i></a> 
-    <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
-    <a href="#" class="youtube"><i class="fa fa-youtube"></i></a> </div>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <div class="icon-bar">
-    </div>
+<div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
+  <div class="col-md-12 px-0">
+    <h1 class="display-4 font-italic" align="center">BLAH BLAH LAND</h1>
+  </div>
 </div>
-<!-- End of Navigation Bar -->
-
-<!-- Wrapping -->
-<div class="big-wrapper">
 
 <!-- Header -->
-<div class="header-cover section bg-dark-light no-padding">
-    <div class="header section" style="background-image: url(https://images.unsplash.com/photo-1510511450816-30c68106b199?ixlib=rb-0.3.5&s=b8ec5e44eabbf55182615ee0e65be14e&auto=format&fit=crop&w=1950&q=80);">
-    <div class="header-inner section-inner">
-    <div class="blog-info">
-    <h2 class="blog-title"><a href="home" rel="home">Blah Blah Land</a></h2></div></div></div>
-</div>
-<!-- End of Header -->
 
-<!-- End of Wrapping -->
-</div>
+<h2>Register</h2>
 
-<!-- Wrapping -->
-<div class="wrapper section-inner">
+<!-- Content -->
 
-<!-- Article -->
-<div class="content left">
-    <div class="posts">
-    <div class="post-header">
-        <h1 class="post-title">Tell us a bit about yourself...</a></h1>
-        <p>Please register to use the site by completing the fields below:<br><br>
-        <div class="container py-3">
+<div class="container py-3">
     <form method="post">
   <div class="form-row">
     <div class="col-md-4 mb-3">
@@ -79,7 +31,7 @@
       <label for="validationServerPassword">Password:</label>
 <!--      <div class="input-group">-->
           <input type="password" class="form-control" id="validationServerUsername" placeholder="Password" aria-describedby="inputGroupPrepend3" required name="password">
-    </div><br>
+      </div>
     </div>
   </div>
   <div class="form-row">
@@ -103,127 +55,3 @@
   <button class="btn btn-primary" type="submit">Submit</button>
 </form>
 </div>
-        
-</div>
-        <div class="clear"></div><br>
-    <div class="clear"></div>
-    
-    
-<!-- End of Article -->
-
-    </div>
-<!-- End of Meta Tags -->
-
-    </div>
-<!-- End of Posts -->
-
-    </div>
-<!-- End of content to the left -->
-
-<!-- Sidebar -->
-<!-- Other Articles -->
-    <div class="sidebar right" role="complementary">
-    <div class="widget widget_recent_entries"><div class="widget-content">		
-            <h3 class="widget-title">Other Articles</h3>		
-            <ul>                                                                    
-            <li>                   
-                <a href="?controller=blog&action=read&id=17">Carrey On Jim</a><br>
-            <span class="post-date">03-04-2018</span>
-            </li>   
-            <li>
-                <a href="?controller=blog&action=read&id=15">Supergood, actually</a><br>
-            <span class="post-date">03-04-2018</span>
-            </li>   
-            <li>                   
-                <a href="?controller=blog&action=read&id=33">You can't sit with us...</a><br>
-            <span class="post-date">28-01-2018</span>
-            </li>                                       
-            <li>                   
-                <a href="?controller=blog&action=read&id=20">Vote for Pedro!</a><br>
-            <span class="post-date">04-12-2017</span>
-            </li>
-            </ul>
-        </div></div><div class="clear"></div>
-        
-<!-- End of other articles -->
-
-<!-- Categories -->
-    <div class="widget widget_tag_cloud"><div class="widget-content"><h3 class="widget-title">Categories</h3>
-    <div class="tagcloud"><a href="Insert link to category here">Romance</a>
-    <div class="tagcloud"><a href="Insert link to category here">Comedy</a>
-    <div class="tagcloud"><a href="Insert link to category here">Science Fiction</a>
-    <div class="tagcloud"><a href="Insert link to category here">Horror</a>
-    <div class="tagcloud"><a href="Insert link to category here">Action</a>
-    <div class="tagcloud"><a href="Insert link to category here">Thriller</a>
-    <div class="tagcloud"><a href="Insert link to category here">Drama</a>
-    <div class="tagcloud"><a href="Insert link to category here">Mystery</a>
-    <div class="tagcloud"><a href="Insert link to category here">Crime</a>
-    <div class="tagcloud"><a href="Insert link to category here">Animation</a>
-    <div class="tagcloud"><a href="Insert link to category here">Adventure</a>
-    <div class="tagcloud"><a href="Insert link to category here">Fantasy</a>
-
-    </div></div></div></div></div></div></div></div></div></div>
-    <div class="clear"></div>
-<!-- End of Categories -->
-
-    </div>
-<!-- End of Sidebar -->
-
-    </div>
-<!-- End of Wrapper -->
-    
-    <div class="clear"></div>
-<!-- End of page -->
-
-</body>
-</html>
-
-
-   
-
-
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-    <script src="../../../../assets/js/vendor/popper.min.js"></script>
-    <script src="../../../../dist/js/bootstrap.min.js"></script>
-    <script src="../../../../assets/js/vendor/holder.min.js"></script>
-    <script>
-      Holder.addTheme('thumb', {
-        bg: '#55595c',
-        fg: '#eceeef',
-        text: 'Thumbnail'
-      });
-    </script>
-    <br>
-        <footer class="blog-footer">
-      <p>
-        <a href="#">Back to top</a>
-      </p>
-    </footer>
-  </body>
-</html>
-
-
-<!-- Title -->
-
-<title>Register</title>
-
-<!-- Logo -->
-
-<div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
-  <div class="col-md-12 px-0">
-    <h1 class="display-4 font-italic" align="center">BLAH BLAH LAND</h1>
-  </div>
-</div>
-
-<!-- Header -->
-
-<h2>Register</h2>
-
-<!-- Content -->
-
-
