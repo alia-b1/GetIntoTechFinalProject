7af829b24ea0c7d6a7dae830b16047f7152c6435
Penny <pfordred@hotmail.co.uk>
2018-04-22 14:17:12 +0100

    Links to film blogs on Home Page and Film Categories

--- views/pages/home.php
+++ views/pages/home.php
@@ -39,26 +39,26 @@
 
       <div class="nav-scroller py-1 mb-2">
         <nav class="nav d-flex justify-content-between">
-          <a class="p-2 text-muted" href="#">World</a>
-          <a class="p-2 text-muted" href="#">U.S.</a>
-          <a class="p-2 text-muted" href="#">Technology</a>
-          <a class="p-2 text-muted" href="#">Design</a>
-          <a class="p-2 text-muted" href="#">Culture</a>
-          <a class="p-2 text-muted" href="#">Business</a>
-          <a class="p-2 text-muted" href="#">Politics</a>
-          <a class="p-2 text-muted" href="#">Opinion</a>
-          <a class="p-2 text-muted" href="#">Science</a>
-          <a class="p-2 text-muted" href="#">Health</a>
-          <a class="p-2 text-muted" href="#">Style</a>
-          <a class="p-2 text-muted" href="#">Travel</a>
+            <a class="p-2 text-muted" href="#">Romance</a>
+          <a class="p-2 text-muted" href="#">Comedy</a>
+          <a class="p-2 text-muted" href="#">Science Fiction</a>
+          <a class="p-2 text-muted" href="#">Horror</a>
+          <a class="p-2 text-muted" href="#">Action</a>
+          <a class="p-2 text-muted" href="#">Thriller</a>
+          <a class="p-2 text-muted" href="#">Drama</a>
+          <a class="p-2 text-muted" href="#">Mystery</a>
+          <a class="p-2 text-muted" href="#">Crime</a>
+          <a class="p-2 text-muted" href="#">Animation</a>
+          <a class="p-2 text-muted" href="#">Adventure</a>
+          <a class="p-2 text-muted" href="#">Fantasy</a>
         </nav>
       </div>
 
       <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
         <div class="col-md-6 px-0">
-          <h1 class="display-4 font-italic">Title of a longer featured blog post</h1>
-          <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.</p>
-          <p class="lead mb-0"><a href="#" class="text-white font-weight-bold">Continue reading...</a></p>
+          <h1 class="display-4 font-italic">Live long and prosper...</h1>
+          <p class="lead my-3">I am proud to call my self a Trekkie, I attend conventions, dress up and I even have "Live Long and Prosper" tattooed on my back. My family however, despite my best efforts, will watch the occasional episode but have never embraced the Star Trek world as I have. My husband took me along to the premier of Star Trek in London last night and thank you JJ you converted him in one film, I have been trying for 25 years!</p>
+          <p class="lead mb-0"><a href="?controller=blog&action=read&id=24" class="text-white font-weight-bold">Continue reading...</a></p>
         </div>
       </div>
 
@@ -66,13 +66,13 @@
         <div class="col-md-6">
           <div class="card flex-md-row mb-4 box-shadow h-md-250">
             <div class="card-body d-flex flex-column align-items-start">
-              <strong class="d-inline-block mb-2 text-primary">World</strong>
+              <strong class="d-inline-block mb-2 text-primary"></strong>
               <h3 class="mb-0">
-                <a class="text-dark" href="#">Featured post</a>
+                <a class="text-dark" href="?controller=blog&action=read&id=17">Carrey on Jim</a>
               </h3>
-              <div class="mb-1 text-muted">Nov 12</div>
-              <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-              <a href="#">Continue reading</a>
+              <div class="mb-1 text-muted">Most Popular</div>
+              <p class="card-text mb-auto">Ace Ventura is the kind of comedy that stems directly from a TV show, and in this case, that show was probably In Living Color, where Carrey got most of his publicity before he became famous as a movie actor.</p>
+              <a href="?controller=blog&action=read&id=17">Continue reading</a>
             </div>
             <img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Card image cap">
           </div>
@@ -80,13 +80,13 @@
         <div class="col-md-6">
           <div class="card flex-md-row mb-4 box-shadow h-md-250">
             <div class="card-body d-flex flex-column align-items-start">
-              <strong class="d-inline-block mb-2 text-success">Design</strong>
+              <strong class="d-inline-block mb-2 text-success">Horror</strong>
               <h3 class="mb-0">
-                <a class="text-dark" href="#">Post title</a>
+                <a class="text-dark" href="#">I always feel like somebody's watching me...</a>
               </h3>
-              <div class="mb-1 text-muted">Nov 11</div>
-              <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-              <a href="#">Continue reading</a>
+              <div class="mb-1 text-muted"></div>
+              <p class="card-text mb-auto">Inspired by 70's and 80's horror, it follows is a refreshing psychological horror film with a simple premise and a chilling concept. The cinematography is electrifying, every shot is beautiful and the score holds brilliance, it carries a very obvious John Carpenter vibe to it...</p>
+              <a href="?controller=blog&action=read&id=33">Continue reading</a>
             </div>
             <img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Card image cap">
           </div>
