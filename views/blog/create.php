81757d429af19c1169f39b75db6563ce31367e1e
Unknown <lara_phillips@hotmail.com>
2018-04-22 20:44:19 +0100

    Revert "merge"
    This reverts commit 3b8ebec7e80eecfa4ab8332eecaa0c4bf4ec0b6c.

--- views/blog/create.php
+++ views/blog/create.php
@@ -1,7 +1,14 @@
 <!-- Title -->
 
+<title>Submit a new article</title>
 
 <!-- Logo -->
+
+<div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
+  <div class="col-md-12 px-0">
+    <h1 class="display-4 font-italic" align="center">BLAH BLAH LAND</h1>
+  </div>
+</div>
 
 <!-- Header -->
 
