f865f7699e07344a94dfcddb85cf4a46f63f92a4
alia-b1 <alia-b@hotmail.co.uk>
2018-04-24 23:40:20 +0100

    Updated some styling for article page

--- views/blog/read.php
+++ views/blog/read.php
@@ -1,8 +1,9 @@
 
 <!-- Content -->
 
-<h2 class="article"> <?php echo $blog->title; ?></h2>
-<p class="container"><?php echo $blog->content; ?></p><br>
+<h2 class="post-title"> <?php echo $blog->title; ?></h2>
+<p class="container"><?php echo $blog->content; ?></p>
+<br>
 <strong><h4>Publish Date:</strong> <?php echo $blog->date_created; ?></h4>
 <br>
 <h2>About this movie</h2>
@@ -10,9 +11,11 @@
     <br>
 <strong><?php echo "Director:</strong> $blog->director" ?>
     <br>
+<<<<<<< Updated upstream
 <strong><?php echo "Release Year:</strong> $blog->release_year" ?>
     <br><br>
-<img src="<?php echo $blog->movie_poster?>" alt="Movie Image">
+<img src="<?php echo $blog->movie_poster?>" alt="Movie Image" style="width:250px;height:300px;">
+<br>
 <br>
 <a href='?controller=blog&action=delete&id=<?php echo $blog->ID; ?>'>Delete Blog Article</a> &nbsp; &nbsp;
 <br>
