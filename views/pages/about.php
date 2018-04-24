e650541dcefae4a817b29486bfd4c9c52982c63b
Unknown <lara_phillips@hotmail.com>
2018-04-25 00:22:49 +0100

    fixed the about page

--- views/pages/about.php
+++ views/pages/about.php
@@ -14,36 +14,55 @@ Meet them below...
 
 <br><br>
 
-<style>
-    .img {
-    position: relative;
-    float: left;
-    width:  200px;
-    height: 200px;
-    background-position: 50% 50%;
-    background-repeat:   no-repeat;
-    background-size:     cover;
-    padding: 10px;
-}
-</style>
 <br><br>
-<div class="img" style="background:url('../images/standard/AliaElsa.png');"></div>
-<h2>Alia</h2>
-Alia currently lives in London and is in her second year of university at UCL, writing an interesting dissertation ;-)<br>
-Alia's favourite film is "Frozen".
+<div class="row mb-5">
+    <div class="col-sm-12 col-md-4">
+        <img height="50" src="views/images/standard/AliaElsa.png" />
+    </div>
+    <div class="col-sm-12 col-md-8">
+        <h2>Alia</h2>
+        <p>
+            Alia currently lives in London and is in her second year of university at UCL, writing an interesting dissertation ;-)<br>
+            Alia's favourite film is "Frozen".
+        </p>
+    </div>
+</div>
 
-<div class="img" style="background:url('../images/standard/PennyGhost.png');"></div>
-<h2>Penny</h2>
-Penny is also in her second year of university, studying at Royal Holloway in Surrey. Penny loves JSON!<br>
-Penny's favourite film is "Ghostbusters".
+<div class="row mb-5">
+    <div class="col-sm-12 col-md-4">
+        <img height="50" src="views/images/standard/PennyGhost.png" />
+    </div>
+    <div class="col-sm-12 col-md-8">
+        <h2>Penny</h2>
+        <p>
+            Penny is also in her second year of university, studying at Royal Holloway in Surrey. Penny loves JSON!<br>
+            Penny's favourite film is "Ghostbusters".
+        </p>
+    </div>
+</div>
 
-<div class="img" style="background:url('../images/standard/LaraGrease.png')"></div>
-<h2>Lara</h2>
-Lara has lived all over the world but currently lives in Henry VIII's old stomping ground.  Lara devotes her time to coding, horses and doggies.<br>
-Lara's favourite film is "Grease".
+<div class="row mb-5">
+    <div class="col-sm-12 col-md-4">
+        <img height="50" src="views/images/standard/LaraGrease.png" />
+    </div>
+    <div class="col-sm-12 col-md-8">
+        <h2>Lara</h2>
+        <p>
+            Lara has lived all over the world but currently lives in Henry VIII's old stomping ground.  Lara devotes her time to coding, horses and doggies.<br>
+            Lara's favourite film is "Grease".
+        </p>
+    </div>
+</div>
 
-<div class="img" style="background:url('../images/standard/LauraET.png');"></div>
-<h2>Laura</h2>
-Laura also loves dogs (especially really ugly ones), and lives in Staines Massive.<br>
-Laura's favourite film is "E.T.".
-
+<div class="row">
+    <div class="col-sm-12 col-md-4">
+        <img height="50" src="views/images/standard/LauraET.png" />
+    </div>
+    <div class="col-sm-12 col-md-8">
+        <h2>Laura</h2>
+        <p>
+            Laura also loves dogs (especially really ugly ones), and lives in Staines Massive.<br>
+            Laura's favourite film is "E.T.".
+        </p>
+    </div>
+</div>    
